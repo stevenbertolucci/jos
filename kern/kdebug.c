ed55786ead32c44392c9886a3a56d75da7707b86
bertolus <bertolus@oregonstate.edu>
2024-04-10 13:08:21 -0700

    Added my solution for lab1 exercise 11

--- kern/kdebug.c
+++ kern/kdebug.c
@@ -181,7 +181,6 @@ debuginfo_eip(uintptr_t addr, struct Eipdebuginfo *info)
 	//	which one.
 	// Your code here.
 
-
 	// Search backwards from the line number for the relevant filename
 	// stab.
 	// We can't just use the "lfile" stab because inlined functions
